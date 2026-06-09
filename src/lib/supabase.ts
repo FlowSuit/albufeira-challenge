6ab11d9d0caa9630aeb853eaafd8edd66e24b9a1
FlowSuit <info@flowsuit.nl>
2026-06-09 15:32:15 +0200

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const url = "https://blveuqrxyfopywbtfuq.supabase.co";
+const url = "https://blveuqrxvyfopywbtfuq.supabase.co";
 const anon = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsdmV1cXJ4dnlmb3B5d2J0ZnVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwMDI0NDcsImV4cCI6MjA5NjU3ODQ0N30.oipRvzt6lGxNK_-p-9u6iEjDwMklDJ88nqmtHcdw3M0";
 
 export const supabase = createClient(url, anon, {
